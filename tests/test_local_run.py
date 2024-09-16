a9701e65c660f9425893528260abc51a3b62f410
Tim Child <timjchild@gmail.com>
2024-09-16 15:50:15 -0700

    basic tests running

--- tests/test_local_run.py
+++ tests/test_local_run.py
@@ -12,17 +12,20 @@ def client() -> LangGraphClient:
 async def test_list_assistants(client: LangGraphClient):
     assistants = await client.assistants.search()
     print(assistants)
-    assert len(assistants) == 1
+    assert len(assistants) > 0
+
+    graph_ids = [assistant["graph_id"] for assistant in assistants]
+    assert set(graph_ids) == {"my_first_agent", "my_second_agent"}
 
 
 async def test_local_client(client):
-    assistant_id = "agent"
     thread = await client.threads.create()
+    assistant = (await client.assistants.search(graph_id="my_second_agent"))[0]
 
     input = {"messages": [{"role": "user", "content": "what's the weather in sf"}]}
     async for chunk in client.runs.stream(
         thread["thread_id"],
-        assistant_id,
+        assistant_id=assistant["assistant_id"],
         input=input,
         stream_mode="updates",
     ):
